aaaa839d8ba3dcc0618283e7e3f9c403347bb493
Mohit Joshi <mohitjoshi.ngr.2002@gmail.com>
2022-09-04 13:01:16 +0530

    wait

--- assignment1/program5.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-printf("The length of the string ");
-int x = printf("Mohit");
-printf(" is: %d",x);
-return 0;
-}
--- assignment1/program7.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-int x = 37;
-printf("%cd",x);
-return 0;
-}
